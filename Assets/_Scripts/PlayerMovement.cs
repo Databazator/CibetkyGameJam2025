e24e1896c408b3ccf12f8e71c8c14d45c7e0e062
Databazator <bleskaceq9@gmail.com>
2025-11-28 19:33:06 +0100

    player controller start

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInputActions _actions;
+    private InputAction _movementAction;
+    private InputAction _lookAction;
+    private InputAction _attackAction;
+    private InputAction _dashAction;
+    public float MovementSpeed;
+    public const float GRAVITY = 10f;
+    
+    private CharacterController _charController;
+    public Vector3 LookDirection = Vector3.forward;
+
+    public Transform TrackingDebug;
+
+    public float ControllerLookDeadzone = 0.35f;
+    private bool _isMKBUsed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    float RemapRange(float value, float min_a, float max_a, float min_b, float max_b)
+    {
+        return min_b + (value - min_a) * (max_b - min_b) / (max_a - min_a);
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleLookDirection();
+
+        HandleAttack();
+        HandleDash();
+    }
+
+    void HandleMovement()
+    {
+        Vector2 input = _movementAction.ReadValue<Vector2>();
+
+        Vector3 movementInput = new Vector3(input.x, 0, input.y);
+
+        Vector3 movementVector = movementInput * MovementSpeed * Time.deltaTime;
+        _charController.Move(movementVector);
+
+        if(!_charController.isGrounded)
+        {
+            Vector3 gravityForce = Vector3.down * GRAVITY * Time.deltaTime;
+            _charController.Move(gravityForce);
+        }
+    }
+
+    void HandleAttack()
+    {
+
+    }
+
+    void HandleDash()
+    {
+
+    }
+
+
+
+    void HandleLookDirection()
+    {
+        if(_isMKBUsed)
+        {
+            LookDirection = GetMouseLookDirection();
+        }
+        else
+        {
+            LookDirection = GetGamepadLookDirection();
+        }
+
+        this.transform.rotation = Quaternion.LookRotation(LookDirection, Vector3.up);
+    }
+
+    Vector3 GetMouseLookDirection()
+    {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Camera mainCam = Camera.main;
+        float playerCamDist = Vector3.Distance(mainCam.transform.position, this.transform.position);
+
+        Vector2 mouseScreenPosNormalized = new Vector2(
+            Mathf.Clamp01(RemapRange(mouseScreenPos.x, 0, mainCam.pixelWidth, 0f, 1f)),
+            Mathf.Clamp01(RemapRange(mouseScreenPos.y, 0, mainCam.pixelHeight, 0f, 1f))
+            );
+
+        float screenYDistMult = RemapRange(mouseScreenPosNormalized.y, 0, 1, 0.25f, 1.75f);
+        playerCamDist *= screenYDistMult;
+        Debug.Log($"mousePos: {mouseScreenPos}, normalized: {mouseScreenPosNormalized}, multedY: {screenYDistMult}");
+        Vector3 mouseWorldSpace = mainCam.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, playerCamDist));
+
+        mouseWorldSpace = new Vector3(mouseWorldSpace.x, transform.position.y, mouseWorldSpace.z);
+        TrackingDebug.position = mouseWorldSpace;
+
+        return mouseWorldSpace - transform.position;
+    }
+
+    Vector3 GetGamepadLookDirection()
+    {
+        Vector2 input = _lookAction.ReadValue<Vector2>();
+
+        TrackingDebug.position = transform.position + LookDirection;
+
+        if (input == Vector2.zero || input.magnitude <= ControllerLookDeadzone) // if no input, stay on previous look dir
+            return LookDirection;
+        return new Vector3(input.x, 0f, input.y);
+    }
+
+    private void Awake()
+    {
+        _charController = GetComponent<CharacterController>();
+
+        _actions = new PlayerInputActions();
+        _movementAction = _actions.Player.Move;
+        _lookAction = _actions.Player.Look;
+        _attackAction = _actions.Player.Attack;
+        _dashAction = _actions.Player.Dash;
+        InputSystem.onActionChange += InputSystem_onActionChange;
+    }
+
+    private void InputSystem_onActionChange(object obj, InputActionChange change)
+    {
+        if(change == InputActionChange.ActionStarted || change == InputActionChange.ActionPerformed)
+        {
+            InputAction recievedAction = (InputAction)obj;
+            InputDevice device = recievedAction.activeControl.device;
+
+            _isMKBUsed = device.name.Equals("Keyboard") || device.name.Equals("Mouse");
+        }
+    }
+
+    private void OnEnable()
+    {
+        _actions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _actions.Player.Disable();
+    }
+}
