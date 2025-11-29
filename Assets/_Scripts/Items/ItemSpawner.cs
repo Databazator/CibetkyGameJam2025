680624d0ba92e6b6f6d94e42ea17643e022ba51e
Jakub Mifek <jakubmifek@gmail.com>
2025-11-29 10:02:39 +0100

    Cleanup

--- Assets/_Scripts/Items/ItemSpawner.cs
+++ Assets/_Scripts/Items/ItemSpawner.cs
@@ -7,13 +7,13 @@ public class ItemSpawner : MonoBehaviour
 
     public List<Item> items = new List<Item>();
     public GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         player = GameObject.FindWithTag("Player");
     }
 
-    void pickItem(int index)
+    public void pickItem(int index)
     {
         player.GetComponent<PlayerInventory>().AddItem(items[index]);
     }
