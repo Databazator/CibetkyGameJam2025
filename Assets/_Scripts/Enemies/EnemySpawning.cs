82c9738904ea3ae8b0143f5d37beb279aa23af5d
Jakub Mifek <jakubmifek@gmail.com>
2025-11-29 01:57:36 +0100

    LevelConcept

--- Assets/_Scripts/Enemies/EnemySpawning.cs
+++ Assets/_Scripts/Enemies/EnemySpawning.cs
@@ -3,22 +3,43 @@ using UnityEngine;
 public class EnemySpawning : MonoBehaviour
 {
     public int numberOfEnemies = 5;
+    private int remainingEnemies;
     public GameObject enemyPrefab;
     public GameObject spawnArea;
+    private bool defeated = false;
+    private RoomLocking roomLocking;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        roomLocking = transform.parent.GetComponentInChildren<RoomLocking>();
+    }
+
+    public void StartWave()
     {
         SpawnEnemies();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsDefeated()
     {
-        
+        return defeated;
     }
 
-    public void SpawnEnemies()
+    public void DefeatEnemy()
+    {
+        remainingEnemies--;
+        Debug.Log("Enemy defeated in spawner, remaining: " + remainingEnemies + ".");
+        if (remainingEnemies <= 0)
+        {
+            defeated = true;
+            Debug.Log("All enemies defeated in spawner.");
+            if (roomLocking != null)
+            {
+                roomLocking.SpawnerDefeated();
+            }
+        }
+    }
+
+    private void SpawnEnemies()
     {
         for (int i = 0; i < numberOfEnemies; i++)
         {
@@ -34,6 +55,9 @@ public class EnemySpawning : MonoBehaviour
             behavior.roomTopRight = spawnArea.transform.position + spawnArea.transform.localScale;
             // Set name
             behavior.enemyName = "Enemy_" + i;
+            var health = enemy.GetComponent<EnemyHealth>();
+            health.spawner = this;
         }
+        remainingEnemies = numberOfEnemies;
     }
 }
